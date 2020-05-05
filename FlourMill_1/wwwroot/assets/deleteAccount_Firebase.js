f958c3a3874b17b99324c43ae8f38d75f4ff2977
killerm250m <killerm250m@gmail.com>
2020-05-05 03:49:15 +0300

    update

--- FlourMill_1/wwwroot/assets/deleteAccount_Firebase.js
+++ FlourMill_1/wwwroot/assets/deleteAccount_Firebase.js
@@ -1,12 +1,12 @@
 function deleteBakeryFirebase(id) {
 
 
-    db.collection("Bakerys_Location").doc("DC").delete().then(function () {
+    firebase.firestore().collection("Bakerys_Location").doc("DC").delete().then(function () {
         console.log("Account successfully deleted! Location Bakery");
     }).catch(function (error) {
         console.error("Error removing document: ", error);
     });
-    db.collection("Users").doc("DC").delete().then(function () {
+    firebase.firestore().collection("Users").doc("DC").delete().then(function () {
         console.log("Account successfully deleted! Bakery ");
     }).catch(function (error) {
         console.error("Error removing document: ", error);
@@ -15,7 +15,7 @@ function deleteBakeryFirebase(id) {
 }
 
 function deleteAdminFirebase(id) {
-    db.collection("flourmill_location").doc(id).delete().then(function () {
+    firebase.firestore().collection("flourmill_location").doc(id).delete().then(function () {
         console.log("Account successfully deleted! Admin");
     }).catch(function (error) {
         console.error("Error removing document:", error);
@@ -23,13 +23,13 @@ function deleteAdminFirebase(id) {
 
 }
 function deleteTruckDriver(id) {
-    db.collection("user_locations").doc("DC").delete().then(function () {
+    firebase.firestore().collection("user_locations").doc("DC").delete().then(function () {
         console.log("Account successfully deleted! location TruckDriver");
     }).catch(function (error) {
         console.error("Error removing document: ", error);
     });
 
-    db.collection("Users").doc("DC").delete().then(function () {
+    firebase.firestore().collection("Users").doc("DC").delete().then(function () {
         console.log("Account successfully deleted! Truck Driver");
     }).catch(function (error) {
         console.error("Error removing document: ", error);
