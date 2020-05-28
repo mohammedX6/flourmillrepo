0c22e796c8aed0bfc9c717970ab9bf8849c4d654
killerm250m <killerm250m@gmail.com>
2020-05-28 18:52:25 +0300

    updated password length

--- FlourMill_1/Dtos/BakeryRegisterDTO.cs
+++ FlourMill_1/Dtos/BakeryRegisterDTO.cs
@@ -9,7 +9,7 @@ namespace FlourMill_1.Dtos
         public string Username { get; set; }
 
         [Required]
-        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify password between 4 and 8 char")]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "You must specify password between 4 and 20 char")]
         public string Password { get; set; }
 
         [Required]
